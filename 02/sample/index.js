0af371330a8944f4bff9f9e30ef8d678361867e7
adamhan <806514395@qq.com>
2019-07-24 21:27:20 +0800

    update

--- 02/sample/index.js
+++ 02/sample/index.js
@@ -1,18 +1,27 @@
 const Koa = require("koa");
 const app = new Koa();
+//每一个use都是中间件  
+
 app.use((ctx, next) => {
   ctx.body = [
     { name: "tom" }
   ]
+
+  //next 表示使用下一个中间件 
   next()
+
 
   ctx.body.push({
     name: "abc"
   })
+
+
 })
 app.use((ctx, next) => {
   ctx.body.push({
     name: "jerry"
   })
+
 })
+
 app.listen(3000)
